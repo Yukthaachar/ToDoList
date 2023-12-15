806f994228de6e78897dec6280f23a51566e45dc
Yukthaachar <149451003+Yukthaachar@users.noreply.github.com>
2023-12-15 15:47:31 +0530

    Delete todolist directory

--- todolist/todolist.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE tasks (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    task_name VARCHAR(255) NOT NULL,
-    task_description TEXT,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
